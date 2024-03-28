34808119afe3bb38d2a3dce8a25ff465ec16335d
Aagboola <A_Agboola18@cranford.hounslow.sch.uk>
2024-03-28 15:02:34 +0000

    setting up project made the excel spreadsheet to the files

--- xlsxmaker.py
+++ xlsxmaker.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("Invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath)
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    Filename = Path(filepath).stem
+
+    pdf.set_font(family="Helvetica",style="B", size=16)
+    pdf.cell(w=0, h=12, txt="Invoice number: ")
