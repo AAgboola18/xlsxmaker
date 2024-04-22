8b76a64953d6a9e24d2dddb16b79b8ecc57a8df9
A_Agboola18 <A_Agboola18@cranford.hounslow.sch.uk>
2024-04-22 13:48:49 +0100

    day 2

--- xlsxmaker.py
+++ xlsxmaker.py
@@ -7,6 +7,7 @@ filepaths = glob.glob("Invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath)
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
@@ -14,6 +15,11 @@ for filepath in filepaths:
     invoice_date, invoice_number = filename.split("-")
 
     pdf.set_font(family="Helvetica", style="B", size=16)
-    pdf.cell(w=0, h=12, txt=f"Invoice number: {invoice_number}", ln=1)
-
+    pdf.cell(w=0, h=12, txt=f"Invoice number: {invoice_number}", ln=1,  align="R")
+    headers = df.columns
+    pdf.cell(w=30, h=8, txt=headers[0], border=1)
+    pdf.cell(w=30, h=8, txt=headers[1], border=1)
+    pdf.cell(w=30, h=8, txt=headers[2], border=1)
+    pdf.cell(w=30, h=8, txt=headers[3], border=1)
+    pdf.cell(w=30, h=8, txt=headers[4], border=1)
     pdf.output(f"PDFs/{filename}.pdf")
