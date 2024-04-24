59beb9fe4a4e5d59324c2d9aad48b4af092dfc9a
A_Agboola18 <A_Agboola18@cranford.hounslow.sch.uk>
2024-04-24 11:27:56 +0100

    generating content

--- xlsxmaker.py
+++ xlsxmaker.py
@@ -21,9 +21,20 @@ for filepath in filepaths:
 
     pdf.set_font(family="Helvetica", style="B", size=12)
     pdf.cell(w=30, h=8, txt=headers[0], border=1)
-    pdf.cell(w=30, h=8, txt=headers[1], border=1)
-    pdf.cell(w=30, h=8, txt=headers[2], border=1)
-    pdf.cell(w=30, h=8, txt=headers[3], border=1)
-    pdf.cell(w=30, h=8, txt=headers[4], border=1)
+    pdf.cell(w=59, h=8, txt=headers[1], border=1)
+    pdf.cell(w=43, h=8, txt=headers[2], border=1)
+    pdf.cell(w=33, h=8, txt=headers[3], border=1)
+    pdf.cell(w=25, h=8, txt=headers[4], border=1, ln=1)
+
+    for index,row in df.iterrows():
+        pdf.set_font(family="Helvetica", size=12)
+        pdf.set_text_colour(80, 80, 80)
+        pdf.cell(w=59, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=43, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=33, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=25, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=25, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+
 
     pdf.output(f"PDFs/{filename}.pdf")
